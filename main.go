8530cfc6bfbb2583d740d634df920c1fa26621d9
Alexey Yakovlev <mrlexus21@yandex.ru>
2024-08-27 13:14:25 +0300

    Урок 2. Циклы, ветвления

--- main.go
+++ main.go
@@ -1,52 +1,57 @@
 package main
 
-import (
-	"log"
-	"net/http"
-)
+import "fmt"
 
 func main() {
-	/* var a int8 = 127
-	var b int16 = 32767
-	var c int32 = 30
-	var d int64 = 20
-	var e uint8 = 10
-	var f uint16
-	var g uint32
-	var h uint64
-	var i byte //uint8
-	var j rune //int32
-	var k int  // int32 or int64
-	var l uint // uint32 or uint64
-	var a1 float32 = 1.8
-	var b1 float64 = 1.8
-	var c1 complex64 = 1 + 2i
-	var c2 complex128 = 4 + 90i
+	defer fmt.Println(" world")
+	fmt.Println("Hello")
+	/*s1, s2 := test()
+	fmt.Println(s1, s2)*/
 
-	var b23 bool = true
+	sum := 0
+	//for i := 0; i < 10; i++ {
+	for sum < 1000 { //while
+		sum += 10
+	}
+	fmt.Println(sum)
 
-	var name string = "Hello World"
-	fmt.Println(name)
+	a := 0
+	for a < 200 {
+		//if a == 100 {
+		//	fmt.Println("a is 100")
+		//}
 
-	var (
-		name1 = "Arthur"
-		age1  = 18
-	)
-	fmt.Println(name1)
-	fmt.Println(age1)
+		switch i := isTest(a); i {
+		case 1:
+			fmt.Println("a = 2")
+		case 2:
+			fmt.Println("a = 3")
+		default:
+			fmt.Printf("unknown a=%d\n", a)
+		}
 
-	var name2, age2 = "Arthur", 32
-	fmt.Println(name2)
-	fmt.Println(age2)
-
-	cc := fmt.Sprintf("My name is %s and age %d", name2, age2)
-	fmt.Println(cc)*/
-	http.HandleFunc("/", Handler)
-	log.Println("Start HTTP server on port 8001")
-	log.Fatal(http.ListenAndServe(":8081", nil))
+		/*if i := isTest(a); i == 1 {
+			fmt.Println("a = 2")
+		} else if i == 2 {
+			fmt.Println("a = 3")
+		} else {
+			fmt.Printf("unknown a=%d\n", a)
+		}*/
+		a++
+	}
 }
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, another world!\n"))
+/*func test() (a, b string) {
+	a = "hello"
+	b = "world"
+	return a, b
+}*/
 
+func isTest(a int) int {
+	if a == 2 {
+		return 1
+	} else if a == 3 {
+		return 2
+	}
+	return 3
 }
